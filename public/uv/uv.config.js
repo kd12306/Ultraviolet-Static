8ab36a0ff2afc5a171f2980fe5370f5a6aa04bee
kd12306 <123005110+kd12306@users.noreply.github.com>
2023-12-14 09:32:19 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "bypassbandits.io",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
