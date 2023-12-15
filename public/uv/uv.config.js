1c4f707fc35944213e7559c84220342aaec91f5e
kd12306 <123005110+kd12306@users.noreply.github.com>
2023-12-15 08:45:49 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "www.bypassbandits.io",
+  prefix: "/uv/service/",
   bare: "https://uv.holyubofficial.net/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
